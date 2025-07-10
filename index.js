5f619dcad72b9801690ce006649dcb0adc0f7816
Abdulrahman Binaspro <binaspro@gmail.com>
2025-07-10 12:43:18 +0300

    update

--- index.js
+++ index.js
@@ -340,10 +340,6 @@ app.get("/quran-teacher-report/survey", authenticateToken, async (req, res) => {
       1002: "Cumar Cabdikaafi Axmed",
       1003: "Saadaq Shariif Faarax",
       1004: "Xasan Salaad Tarabi",
-      1005: "Cabdullahi Aadan",
-      1006: "Maxamed Cumar",
-      1007: "Maxamuud Sidow",
-      1008: "Khaalid Cabdullaahi",
     };
 
     // Initialize result map with all 8 entries
@@ -352,10 +348,6 @@ app.get("/quran-teacher-report/survey", authenticateToken, async (req, res) => {
       1002: { agent: agentMap[1002], android: 0, ios: 0 },
       1003: { agent: agentMap[1003], android: 0, ios: 0 },
       1004: { agent: agentMap[1004], android: 0, ios: 0 },
-      1005: { agent: agentMap[1005], android: 0, ios: 0 },
-      1006: { agent: agentMap[1006], android: 0, ios: 0 },
-      1007: { agent: agentMap[1007], android: 0, ios: 0 },
-      1008: { agent: agentMap[1008], android: 0, ios: 0 },
       otherAgents: { agent: "Other Agents", android: 0, ios: 0 },
       social: { agent: "Social Media", android: 0, ios: 0 },
       friend: { agent: "Friend", android: 0, ios: 0 },
